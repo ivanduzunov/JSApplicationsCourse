261a966dadcaa61c570b06a03adfa0c1810c9ab5
ivanduzunov <ivan.d.uzunov@gmail.com>
2018-08-29 15:47:21 +0300

    Exam12August initial commit. templates ready.

---
+++ Exam12August/scripts/app.js
@@ -0,0 +1,25 @@
+const handlers = {};
+
+$(() => {
+    // Define routes here using Sammy.js
+    const app = Sammy('#container', function () {
+        this.use('Handlebars', 'hbs');
+
+        /*this.get('index.html', handlers.getLoginPage); //loginPage will be shown at first
+
+        this.get('#/login', handlers.getLoginPage);// loginPage
+        this.post('#/login', handlers.loginUser);
+        this.get('#/register', handlers.getRegisterPage);//registerPage
+        this.post('#/register', handlers.registerUser);
+
+        this.get('#/home', handlers.getHomePage);//homePage with listing of active flights
+        this.get('#/flights/add', handlers.getFlightAddPage);//get flights add page
+        this.post('#/flights/add', handlers.AddFlight);//post addFlight
+
+        this.get('#/flights/:id', handlers.getFlightDetailsPage);//get flights add page
+
+        this.get('#/flights/edit/:id', handlers.getFlightEditPage);//get flights edit page
+        this.post('#/flights/edit', handlers.editFlight);//post flights edit*/
+    });
+    app.run();
+});
---
+++ Exam12August/scripts/handlers/authHandler.js
@@ -0,0 +1,75 @@
+
+/*handlers.getLoginPage = function (ctx) {
+    ctx.isAuthorized = auth.isAuth;
+
+    ctx.loadPartials({
+        loginForm: './templates/forms/loginForm.hbs',
+        navbar: './templates/common/navbar.hbs',
+        footer: './templates/common/footer.hbs'
+    }).then(function () {
+        this.partial('./templates/user/loginView.hbs');
+    })
+};
+
+handlers.getRegisterPage = function (ctx) {
+    ctx.isAuthorized = auth.isAuth;
+
+    ctx.loadPartials({
+        registerForm: './templates/forms/registerForm.hbs',
+        navbar: './templates/common/navbar.hbs',
+        footer: './templates/common/footer.hbs'
+    }).then(function () {
+        this.partial('./templates/user/registerView.hbs');
+    })
+};
+
+handlers.loginUser = function (ctx) {
+    const username = ctx.params.username;
+    const password = ctx.params.password;
+
+    if (username.length === 0) {
+        notify.showError('Username is requried!');
+    } else if (password.length === 0) {
+        notify.showError('Password is requried!');
+    } else {
+        auth.login(username, password)
+            .then((userData) => {
+                auth.saveSession(userData);
+                notify.showInfo('Login successful.');
+                ctx.redirect('#/home');
+            })
+            .catch(notify.handleError);
+    }
+};
+
+handlers.registerUser = function (ctx) {
+    const username = ctx.params.username;
+    const password = ctx.params.password;
+    const passwordCheck = ctx.params.passwordCheck;
+
+    if(username.length < 5){
+        notify.showError('Username must be at least 5 chars long!');
+    } else if (password.length === 0) {
+        notify.showError('Password must be non-empty!');
+    } else if (password !== passwordCheck) {
+        notify.showError('Both passwords must match!');
+    } else {
+        auth.register(username, password)
+            .then((userData) => {
+                auth.saveSession(userData);
+                notify.showInfo('User registration successful.');
+                ctx.redirect('#/home');
+            })
+            .catch(notify.handleError)
+    }
+};
+
+
+handlers.logout = function (ctx) {
+    auth.logout()
+        .then(() => {
+            sessionStorage.clear();
+            notify.showInfo('Logout successful.');
+            ctx.redirect('#/home');
+        })
+};*/
